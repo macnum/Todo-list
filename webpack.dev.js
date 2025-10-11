0a96dc099a37db131068db768edae4d2c6b9f479
macnum <w.damilola@yahoo.com>
2025-10-11 22:34:41 +0100

    feat: add webpack config for dev and production

---
+++ webpack.dev.js
@@ -0,0 +1,14 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["src/**/*.{js,jsx,ts,tsx}", "public/**/*.{html,css}"],
+    hot: true,
+    open: true,
+    port: 3000,
+    historyApiFallback: true,
+  },
+});
